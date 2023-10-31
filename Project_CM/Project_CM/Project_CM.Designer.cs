9c8e286b41864a4dc5c86913e9f53a5c40958740
Zohanubis <phdh141003@gmail.com>
2023-10-31 23:35:48 +0700

    31/10

---
+++ Project_CM/Project_CM/Project_CM.Designer.cs
@@ -0,0 +1,49 @@
+﻿
+namespace Project_CM
+{
+    partial class Project_CM
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Project_CM
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Project_CM";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Project_CM_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
